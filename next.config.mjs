3943fa5b6c2a468a51b92e08a77fd2bd7c3d9b95
aayanqazi <aayanqazi@yahoo.com>
2024-08-01 16:43:26 +0500

    Implemented rewrites for domain to sub domain

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,24 @@ const nextConfig = {
     domains: ['mekoy17nau9fhmpf.public.blob.vercel-storage.com','www.chatbot.com','btrendy.co'],
   },
   swcMinify: true,
+  rewrites() {
+    return {
+        beforeFiles: [
+            // if the host is `app.acme.com`,
+            // this rewrite will be applied
+            {
+                source: '/:path*',
+                has: [
+                    {
+                        type: 'host',
+                        value: 'chatpad.co',
+                    },
+                ],
+                destination: 'https://app.chatpad.co/:path*',
+            },
+        ]
+    }
+  }
 };
 
 export default nextConfig;
