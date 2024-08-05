42facac6beda90ba4274fee50d7eb54527a6e098
aayanqazi <aayanqazi@yahoo.com>
2024-08-06 00:41:41 +0500

    Added before files

--- next.config.mjs
+++ next.config.mjs
@@ -7,57 +7,22 @@ const nextConfig = {
     domains: ['mekoy17nau9fhmpf.public.blob.vercel-storage.com','www.chatbot.com','btrendy.co'],
   },
   swcMinify: true,
-  async redirects() {
-    return [
-      {
-        source: '/:path*', // Match any path
-        has: [
-          {
-            type: 'host',
-            value: 'www.chatpad.co',
-          },
-        ],
-        destination: 'https://app.chatpad.co/:path*', // Redirect to app.chatpad.co
-        permanent: true, // Permanent redirect
-        // Condition to exclude root path
-        missing: [
-          {
-            type: 'header',
-            key: 'x-do-not-redirect',
-          },
-        ],
-      },
-    ];
-  },
-  async headers() {
-    return [
-      {
-        source: '/',
-        headers: [
-          {
-            key: 'x-do-not-redirect',
-            value: 'true',
-          },
-        ],
-      },
-    ];
+  rewrites() {
+    return {
+      beforeFiles: [
+            {
+                source: '/:path((?!^$).*)',
+                has: [
+                    {
+                        type: 'host',
+                        value: 'www.chatpad.co',
+                    }
+                ],
+                destination: 'https://app.chatpad.co/:path*',
+            },
+        ]
+    }
   }
-  // rewrites() {
-  //   return {
-  //     afterFiles: [
-  //           {
-  //               source: '/:path((?!^$).*)',
-  //               has: [
-  //                   {
-  //                       type: 'host',
-  //                       value: 'www.chatpad.co',
-  //                   }
-  //               ],
-  //               destination: 'https://app.chatpad.co/:path*',
-  //           },
-  //       ]
-  //   }
-  // }
 };
 
 export default nextConfig;
