0f6a73b09a7c6ad3ca7aefece06c43a7156dd2ca
aayanqazi <aayanqazi@yahoo.com>
2024-08-06 00:15:33 +0500

    added afterFiles

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,7 @@ const nextConfig = {
   swcMinify: true,
   rewrites() {
     return {
-      fallback: [
+      afterFiles: [
             {
                 source: '/:path((?!^$).*)',
                 has: [
