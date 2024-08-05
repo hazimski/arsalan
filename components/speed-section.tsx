7313e4613e810e45e8ce22a495b85ceae3371efa
hazimski <130982999+hazimski@users.noreply.github.com>
2024-08-05 19:00:06 +0200

    Update speed-section.tsx

--- components/speed-section.tsx
+++ components/speed-section.tsx
@@ -11,9 +11,23 @@ export function SpeedTabs() {
       content: (
         <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 !text-xl md:!text-4xl font-bold text-white bg-[black] border-[1px] border-[#8F78F1]">
           <p>Product Tab</p>
-<iframe src="https://www.chatpad.co/embed/clxc85c4h0003vo7g2o8vdgam/window?chatbox=false" style="overflow: hidden; height: 80vh; width: 480px; bottom: -30px; border: 2px solid #e2e8f0; border-radius: 0.375rem; box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);" allowfullscreen allow="clipboard-read; clipboard-write" >
-              </div>
-          ),
+          <iframe
+            src="https://www.chatpad.co/embed/clxc85c4h0003vo7g2o8vdgam/window?chatbox=false"
+            style={{
+              overflow: "hidden",
+              height: "80vh",
+              width: "480px",
+              bottom: "-30px",
+              border: "2px solid #e2e8f0",
+              borderRadius: "0.375rem",
+              boxShadow: "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",
+            }}
+            allowFullScreen
+            allow="clipboard-read; clipboard-write"
+          ></iframe>
+          {/* This chatbot is built using https://chatpad.co/ */}
+        </div>
+      ),
     },
     {
       title: "Services",
@@ -58,7 +72,7 @@ export function SpeedTabs() {
   ];
 
   return (
-    <div className="h-[20rem] md:h-[40rem] [perspective:1000px] relative b flex flex-col max-w-5xl mx-auto w-full  items-start justify-start mb-40 mt-20 border-[#8F78F1]">
+    <div className="h-[20rem] md:h-[40rem] [perspective:1000px] relative flex flex-col max-w-5xl mx-auto w-full items-start justify-start mb-40 mt-20 border-[#8F78F1]">
       <Tabs tabs={tabs} />
     </div>
   );
@@ -71,7 +85,7 @@ const DummyContent = () => {
       alt="dummy image"
       width="1000"
       height="1000"
-      className="object-cover object-left-top h-[60%]  md:h-[90%] absolute -bottom-10 inset-x-0 w-[90%] rounded-xl mx-auto"
+      className="object-cover object-left-top h-[60%] md:h-[90%] absolute -bottom-10 inset-x-0 w-[90%] rounded-xl mx-auto"
     />
   );
 };
