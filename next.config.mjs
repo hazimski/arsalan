7321c8b619737e29b1a06e76888529c10c135ec2
aayanqazi <aayanqazi@yahoo.com>
2024-08-06 00:03:58 +0500

    Added some changes

--- next.config.mjs
+++ next.config.mjs
@@ -7,22 +7,27 @@ const nextConfig = {
     domains: ['mekoy17nau9fhmpf.public.blob.vercel-storage.com','www.chatbot.com','btrendy.co'],
   },
   swcMinify: true,
-  // rewrites() {
-  //   return {
-  //     beforeFiles: [
-  //           {
-  //               source: '/:path*',
-  //               has: [
-  //                   {
-  //                       type: 'host',
-  //                       value: 'www.chatpad.co',
-  //                   },
-  //               ],
-  //               destination: 'https://app.chatpad.co/:path*',
-  //           },
-  //       ]
-  //   }
-  // }
+  rewrites() {
+    return {
+      beforeFiles: [
+            {
+                source: '/:path*',
+                has: [
+                    {
+                        type: 'host',
+                        value: 'www.chatpad.co',
+                    },
+                    {
+                      type: 'query',
+                      key: 'path',
+                      value: '^.+$', // Matches only if path is not empty
+                    },
+                ],
+                destination: 'https://app.chatpad.co/:path*',
+            },
+        ]
+    }
+  }
 };
 
 export default nextConfig;
